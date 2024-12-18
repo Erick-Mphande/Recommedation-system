67dd405064419407096a318546490fc4717744e4
Erick-Mphande <148683330+Erick-Mphande@users.noreply.github.com>
2024-12-18 14:29:29 +0200

    Update settings.py

--- IKOMPRJ/settings.py
+++ IKOMPRJ/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-4e+r^il-xwh2eezgk@q4iyv_89%iaj+r)#p3h_@asifsvzcnf7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['quickcart-cf14.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
