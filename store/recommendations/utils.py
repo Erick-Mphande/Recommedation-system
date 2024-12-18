19591388da95ab55d305fab2ded8383686f29f7d
Erick-Mphande <148683330+Erick-Mphande@users.noreply.github.com>
2024-12-18 11:20:09 +0200

    Update utils.py

--- store/recommendations/utils.py
+++ store/recommendations/utils.py
@@ -1,9 +1,13 @@
 import os
 from tensorflow.keras.models import load_model
-from .config import MODEL_PATH
 
-def load_recommendation_model():
-    if os.path.exists(MODEL_PATH):
-        return load_model(MODEL_PATH)
-    else:
-        raise FileNotFoundError(f"Model file not found at {MODEL_PATH}")
+# Update this path to match where your model file is located in the project structure
+model_path = os.path.join(settings.BASE_DIR, r'C:\Users\27728\Desktop\IKOMPRJ\store\recommendations\models\ncf_model.h5')
+model = tf.keras.models.load_model(model_path)
+
+# Load the model
+try:
+    MODEL = load_model(model_path)
+    print("Model loaded successfully!")
+except Exception as e:
+    print(f"Failed to load model: {e}")
